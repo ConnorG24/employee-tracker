8154736265bc4246c096aa540ee612e2fd5dccbe
Fatalis <connman2400@gmail.com>
2022-12-07 21:11:47 -0600

    schema fix

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,24 @@ USE companyInc_db;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    names VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    FOREIGN KEY INT (department_ID)
-    REFERENCES department(id)
-    ON DELETE SET NULL 
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary INT NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
-
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY INT (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY INT (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-
-)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  
+);
