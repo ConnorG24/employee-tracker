fa70e39019bb4f84a93ee3adc591623ca798a426
Fatalis <connman2400@gmail.com>
2022-12-07 20:00:03 -0600

    added seeds for database testing

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department(name)
+VALUES 
+('DPS'),
+('SUPPORT'),
+('Healer');
+
+INSERT INTO role(title, salary department_ID)
+VALUES
+('Samurai', 60000,1),
+('Monk',65000,1),
+('White Mage', 75000,3),
+('Sage',80000,3),
+('Gunbreaker',70000,2),
+('Paladin',72000,2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+('John','Smith',2,4),
+('Joshua','Graham',1,4),
+('Mike','Tyson',2,4),
+('Spike','Spiegel',5,null),
+('Steven','Seagul',1,7),
+('John','Madden',3,8),
+('Jill','Valentine'6,null),
+('Claire','Redfield',4,null);
+
