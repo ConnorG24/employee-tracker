13b80bb25578d8b6b3c276e7f4219e57540e847e
Fatalis <connman2400@gmail.com>
2022-12-07 23:34:46 -0600

    setting up server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const inquirer = require('inquirer')
+const mysql = require('mysql2');
+const cTable = require('console.table')
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
@@ -9,4 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log('Connection Succsessful'));
+  onceConnected();
 });
+
+onceConnected = () =>{
+    //throw up some ascii art 
+    //then prompt questions
+    //add functions to handle question responses
+}
